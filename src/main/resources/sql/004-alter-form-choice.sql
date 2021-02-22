8c5df88d117179b0e357adf123eb12196428bc96
Quentin Périé <quentin.perie@cgi.com>
2021-02-22 10:37:25 +0100

    fix() : Sql nul null default values

--- src/main/resources/sql/004-alter-form-choice.sql
+++ src/main/resources/sql/004-alter-form-choice.sql
@@ -1,6 +1,6 @@
 ALTER TABLE formulaire.form
     ADD date_opening    timestamp without time zone NOT NULL DEFAULT now(),
-    ADD date_ending     timestamp without time zone NOT NULL;
+    ADD date_ending     timestamp without time zone NOT NULL DEFAULT '9999-12-31 23:59:59';
 
 ALTER TABLE formulaire.question_choice
-    ADD position    bigint NOT NULL;
+    ADD position    bigint NOT NULL DEFAULT 0;
