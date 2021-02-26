898fe8436660076ef0ddb3cd36c0a5db1656be91
Quentin Périé <quentin.perie@cgi.com>
2021-02-26 20:09:12 +0100

    feat() : [FOR-15] New progress bar with blue balls

--- src/main/resources/public/ts/controllers/question-responder.ts
+++ src/main/resources/public/ts/controllers/question-responder.ts
@@ -80,9 +80,6 @@ export const questionResponderController = ng.controller('QuestionResponderContr
         if (vm.question.question_type === Types.TIME) { formatTime() }
         vm.distribution = $scope.getDataIf200(await distributionService.get(vm.question.form_id));
 
-        $("#current-bar").css("width", vm.question.position / vm.nbQuestions * 100 + "%")
-            .css("width", "-=8px");
-
         $scope.safeApply();
     };
 
@@ -93,8 +90,6 @@ export const questionResponderController = ng.controller('QuestionResponderContr
         if (prevPosition > 0) {
             $scope.redirectTo(`/form/${vm.question.form_id}/question/${prevPosition}`);
             $scope.safeApply();
-            $scope.form.setFromJson($scope.getDataIf200(await formService.get(vm.question.form_id)));
-            $scope.form.nbQuestions = $scope.getDataIf200(await questionService.countQuestions(vm.question.form_id)).count;
             let question = await questionService.getByPosition(vm.question.form_id, prevPosition);
             $scope.question = question.data;
             init();
@@ -108,8 +103,6 @@ export const questionResponderController = ng.controller('QuestionResponderContr
         if (nextPosition <= vm.nbQuestions) {
             $scope.redirectTo(`/form/${vm.question.form_id}/question/${nextPosition}`);
             $scope.safeApply();
-            $scope.form =  $scope.getDataIf200(await formService.get(vm.question.form_id));
-            $scope.form.nbQuestions = $scope.getDataIf200(await questionService.countQuestions(vm.question.form_id)).count;
             let question = await questionService.getByPosition(vm.question.form_id, nextPosition);
             $scope.question = question.data;
             init();
