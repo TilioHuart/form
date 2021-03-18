fbe7653a03eca89e065c35daeb3111ae9af778fb
Félix Abriac <felixabriac@gmail.com>
2021-03-18 10:15:36 +0100

    fix() : [FOR-75] Fix missing column in SQL file

--- src/main/resources/sql/005-add-table-file.sql
+++ src/main/resources/sql/005-add-table-file.sql
@@ -1,5 +1,5 @@
 ALTER TABLE formulaire.form
-    ALTER COLUMN date_ending    DROP NOT NULL;
+    ALTER COLUMN date_ending DROP NOT NULL;
 
 CREATE TABLE formulaire.response_file (
     id              VARCHAR(36) PRIMARY KEY,
@@ -8,3 +8,6 @@ CREATE TABLE formulaire.response_file (
     type            VARCHAR NOT NULL,
     CONSTRAINT fk_response_id FOREIGN KEY (response_id) REFERENCES formulaire.response (id) ON UPDATE NO ACTION ON DELETE CASCADE
 );
+
+ALTER TABLE formulaire.question
+    ADD COLUMN duplicate_question_id bigint;
