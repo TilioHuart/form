a24444f2995ca4a412762525cb72841d9b0bc4fe
Quentin Périé <quentin.perie@cgi.com>
2021-04-27 15:34:56 +0200

    feat() : [FOR-32] Complete QCU & QCM results (graphs, responsive, etc.)

--- src/main/resources/public/ts/controllers/form-results.ts
+++ src/main/resources/public/ts/controllers/form-results.ts
@@ -1,13 +1,15 @@
-import {ng, template} from 'entcore';
+import {idiom, ng, template} from 'entcore';
+import * as ApexCharts from 'apexcharts';
 import {
     Distributions, DistributionStatus,
     Form,
-    Question, QuestionChoices,
+    Question, QuestionChoice, QuestionChoices,
     Questions,
     Responses,
     Types
 } from "../models";
 import {Mix} from "entcore-toolkit";
+import {ColorUtils} from "../utils/color";
 
 interface ViewModel {
     types: typeof Types;
@@ -17,10 +19,14 @@ interface ViewModel {
     distributions: Distributions;
     form: Form;
     nbResults: number;
-    nbResultsDisplay: number;
+    nbLines: number;
+    nbLinesDisplay: number;
     nbQuestions: number;
     last: boolean;
     navigatorValue: number;
+    singleAnswerResponseChart: any;
+    multipleAnswerResponseChart: any;
+    colors: string[];
     display: {
         lightbox: {
             download: boolean;
@@ -40,6 +46,7 @@ interface ViewModel {
 
 export const formResultsController = ng.controller('FormResultsController', ['$scope', '$rootScope',
     function ($scope, $rootScope) {
+        let paletteColors = ['#FFE4CC','#FF8500','#9C5000']; // Beige, Orange, Marron
 
         const vm: ViewModel = this;
         vm.types = Types;
@@ -49,10 +56,14 @@ export const formResultsController = ng.controller('FormResultsController', ['$s
         vm.distributions = new Distributions();
         vm.form = new Form();
         vm.nbResults = 0;
-        vm.nbResultsDisplay = 10;
+        vm.nbLines = 0;
+        vm.nbLinesDisplay = 10;
         vm.nbQuestions = 1;
         vm.last = false;
         vm.navigatorValue = 1;
+        vm.singleAnswerResponseChart = null;
+        vm.multipleAnswerResponseChart = null;
+        vm.colors = [];
         vm.display = {
             lightbox: {
                 download: false
@@ -70,17 +81,35 @@ export const formResultsController = ng.controller('FormResultsController', ['$s
             let validDistribIds : any = vm.results.all.map(r => r.distribution_id);
             vm.distributions.all = vm.distributions.all.filter(d => validDistribIds.includes(d.id) && d.status === DistributionStatus.FINISHED);
             vm.nbResults = vm.distributions.all.length;
+            vm.nbLines = vm.nbResults;
             vm.nbQuestions = $scope.form.nbQuestions;
             vm.last = vm.question.position === vm.nbQuestions;
-            if (vm.question.question_type == vm.types.SINGLEANSWER ||
-                vm.question.question_type == vm.types.MULTIPLEANSWER) {
+            if (vm.question.question_type == vm.types.SINGLEANSWER || vm.question.question_type == vm.types.MULTIPLEANSWER) {
+                // Count responses for each choice
                 for (let result of vm.results.all) {
                     for (let choice of vm.question.choices.all) {
-                        if (result.choice_id == choice.id) {
+                        if (result.choice_id === choice.id) {
                             choice.nbResponses++;
                         }
                     }
                 }
+
+                // Deal with no choice responses
+                let noResponseChoice = new QuestionChoice();
+                noResponseChoice.value = idiom.translate('formulaire.response.empty');
+                noResponseChoice.nbResponses = vm.results.all.filter(r => !!!r.choice_id).length;
+                vm.question.choices.all.push(noResponseChoice);
+                let choices = vm.question.choices.all.filter(c => c.nbResponses > 0);
+                vm.colors = ColorUtils.interpolateColors(paletteColors, choices.length);
+
+                // Init charts
+                if (vm.question.question_type == vm.types.SINGLEANSWER) {
+                    initSingleAnswerChart(choices);
+                }
+                // if (vm.question.question_type == vm.types.MULTIPLEANSWER) {
+                //     initMultipleAnswerChart();
+                // }
+                vm.nbLines = vm.question.choices.all.length;
             }
             $scope.safeApply();
         };
@@ -133,6 +162,105 @@ export const formResultsController = ng.controller('FormResultsController', ['$s
         vm.goTo = async (position: number) : Promise<void> => {
             $scope.redirectTo(`/form/${vm.question.form_id}/results/${position}`);
         };
+
+        // Charts
+
+        const initSingleAnswerChart = (choices: QuestionChoice[]) : void => {
+            // Fill data
+            let series = [];
+            for (let choice of choices) {
+                series.push(choice.nbResponses);
+            }
+
+            // Fill labels
+            let labels = [];
+            let i18nValue = idiom.translate('formulaire.response');
+            i18nValue = i18nValue.charAt(0).toUpperCase() + i18nValue.slice(1);
+            for (let i = 0; i < vm.question.choices.all.length; i++) {
+                let choice = vm.question.choices.all[i];
+                if (choice.nbResponses > 0) {
+                    if (!!!choice.id) {
+                        labels.push(idiom.translate('formulaire.response.empty'));
+                    }
+                    else {
+                        labels.push(i18nValue + " " + (i+1));
+                    }
+                }
+            }
+
+            // Generate options with labels and colors
+            let options = {
+                chart: {
+                    type: 'pie',
+                    height: 40 * vm.question.choices.all.length
+                },
+                colors: vm.colors,
+                labels: labels
+            }
+
+            // Generate chart with options and data
+            if (vm.singleAnswerResponseChart) {
+                vm.singleAnswerResponseChart.updateSeries(series, true);
+                vm.singleAnswerResponseChart.updateOptions(options, true);
+            }
+            else {
+                var newOptions = JSON.parse(JSON.stringify(options));
+                newOptions.series = series;
+                vm.singleAnswerResponseChart = new ApexCharts(document.querySelector('#singleanswer-response-chart'), newOptions);
+                vm.singleAnswerResponseChart.render();
+            }
+        }
+
+        const initMultipleAnswerChart = () : void => {
+            let choices = vm.question.choices.all.filter(c => c.nbResponses > 0);
+
+            // Fill data
+            let series = [];
+            for (let choice of choices) {
+                series.push(choice.nbResponses);
+            }
+
+            // Fill labels
+            let labels = [];
+            let i18nValue = idiom.translate('formulaire.response');
+            i18nValue = i18nValue.charAt(0).toUpperCase() + i18nValue.slice(1);
+            for (let i = 0; i < choices.length; i++) {
+                labels.push(i18nValue + " " + (i+1));
+            }
+            // labels.push(idiom.translate('formulaire.response.empty'));
+
+            // Generate options with labels and colors
+            let options = {
+                chart: {
+                    type: 'bar'
+                },
+                plotOptions: {
+                    bar: {
+                        borderRadius: 4,
+                        horizontal: true,
+                    }
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                xaxis: {
+                    categories: labels,
+                }
+            }
+
+            // Generate chart with options and data
+            if (vm.multipleAnswerResponseChart) {
+                vm.multipleAnswerResponseChart.updateSeries(series, true);
+                vm.multipleAnswerResponseChart.updateOptions(options, true);
+            }
+            else {
+                var newOptions = JSON.parse(JSON.stringify(options));
+                newOptions.series = [{ data: series }];
+                vm.multipleAnswerResponseChart = new ApexCharts(document.querySelector('#multipleanswer-response-chart'), newOptions);
+                vm.multipleAnswerResponseChart.render();
+            }
+        }
+
 
         init();
 
---
+++ src/main/resources/public/ts/utils/color.ts
@@ -0,0 +1,128 @@
+export class ColorUtils {
+    /**
+     * Check if a given string is valid hexadecimal color
+     * @param color String to test
+     */
+    static isHexaColor = (color: string) : boolean => {
+        return /^#?[0-9A-F]{6}$/i.test(color);
+    }
+
+    /**
+     * Check if a given string is valid RGB color
+     * @param color String to test
+     */
+    static isRgbColor = (color: string) : boolean => {
+        let rgbValues = color.match(/\d+/g).map(Number);
+        if (rgbValues.length < 3 ||rgbValues.length > 4) {
+            return false;
+        }
+        else {
+            for (let i = 0; i < 3; i++) {
+                let value = rgbValues[i];
+                if (value < 0 || value > 255) {
+                    return false;
+                }
+            }
+            if (rgbValues.length === 4) {
+                let value = rgbValues[3];
+                if (value < 0 || value > 1) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    /**
+     * Convert given RGB color into hexadecimal color
+     * @param color RGB color to convert
+     */
+    static rgbToHexa = (color : string) : string => {
+        if (ColorUtils.isHexaColor(color)) {
+            return color;
+        }
+        else if (ColorUtils.isRgbColor(color)) {
+            let rgbValues = color.match(/\d+/g).map(Number);
+            // Convert each element to a base16 string and add zero if we get only one character
+            let hexColor = rgbValues.map(x => {
+                let x16 = x.toString(16);
+                return x16.length === 1 ? "0" + x16 : x16;
+            });
+            return "#" + hexColor.join("");
+        }
+        return null;
+    };
+
+    /**
+     * Convert given hexadecimal color into RGB color
+     * @param color RGB color to convert
+     */
+    static hexaToRgb = (color : string) : string => {
+        if (ColorUtils.isRgbColor(color)) {
+            return color;
+        }
+        else if (ColorUtils.isHexaColor(color)) {
+            let hexValues = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
+            let rgbColor = hexValues ? [
+                parseInt(hexValues[1], 16),
+                parseInt(hexValues[2], 16),
+                parseInt(hexValues[3], 16)
+            ] : null;
+            return "rgb(" + rgbColor.join(",") + ")";
+        }
+        return null;
+    };
+
+    /**
+     * Returns values of an rgb color
+     * @param color String RGB color
+     */
+    static getRgbValues = (color: string) : number[] => {
+        let rgbColor = ColorUtils.hexaToRgb(color);
+        return rgbColor.match(/\d+/g).map(Number);
+    }
+
+
+    /**
+     * Returns a single rgb color interpolation between a list of given colors
+     * @param paletteColors List of colors
+     * @param factor Factor of interpolation
+     */
+    static interpolateColor = (paletteColors: string[], factor: number = 0.5) : number[] => {
+        factor = factor < 0 ? 0 : (factor > 1 ? 1 : factor);
+        let colorValues = [];
+        for (let i = 0; i < paletteColors.length; i++) {
+            colorValues.push(ColorUtils.getRgbValues(paletteColors[i]));
+        }
+
+        let positionOnGradient = (paletteColors.length - 1) * factor;
+        let lowerValue = factor === 1 ? positionOnGradient - 1 : Math.floor(positionOnGradient);
+        let upperValue = factor === 0 ? positionOnGradient + 1 : Math.ceil(positionOnGradient);
+        let color1Values = colorValues[lowerValue];
+        let color2Values = colorValues[upperValue];
+        let newFactor = positionOnGradient / upperValue;
+        let result = [];
+        for (let i = 0; i < color1Values.length; i++) {
+            result.push(Math.round(color1Values[i] + (color2Values[i] - color1Values[i]) * newFactor));
+        }
+        return result;
+    };
+
+    /**
+     * Returns an array of colors interpolated between a list of given colors
+     * @param paletteColors List of colors
+     * @param nbColors Number of colors wanted between the two given colors (included)
+     */
+    static interpolateColors = (paletteColors: string[], nbColors: number) : string[] => {
+        let stepFactor = 1 / (nbColors - 1);
+        let interpolatedColorArray = [];
+
+        for(let i = 0; i < nbColors; i++) {
+            let rgbValues = ColorUtils.interpolateColor(paletteColors, stepFactor * i);
+            let rgbString = "rgb(" + rgbValues.join(",") + ")";
+            interpolatedColorArray.push(ColorUtils.rgbToHexa(rgbString));
+        }
+
+        return interpolatedColorArray;
+    }
+}
