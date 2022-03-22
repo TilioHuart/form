d210b3a9a8f8d4f3aa0ba46b1fc620630a2f45d2
Quentin Périé <quentin.perie@cgi.com>
2022-03-22 15:37:20 +0100

    fix(): [FOR-247] Fix responses navigation

--- src/main/resources/public/ts/utils/formElement.ts
+++ src/main/resources/public/ts/utils/formElement.ts
@@ -1,4 +1,11 @@
-import {Form, FormElement, FormElements, Question, Responses, Section, Distribution} from "../models";
+import {
+    Form,
+    FormElement, FormElements,
+    Question,
+    Response, Responses,
+    Section,
+    Distribution
+} from "../models";
 import {Mix} from "entcore-toolkit";
 
 export class FormElementUtils {
@@ -15,28 +22,28 @@ export class FormElementUtils {
         return formElement instanceof Question;
     };
 
-    static getLastQuestionIds = (formElements: FormElements) : any => {
-        let lastQuestionIds = [];
+    static getLastQuestions = (formElements: FormElements) : any => {
+        let lastQuestions = [];
         for (let e of formElements.all) {
             if (e instanceof Question) {
                 if (e.conditional && e.choices.all.filter(c => !c.next_section_id).length > 0) {
-                    lastQuestionIds.push(e.id);
+                    lastQuestions.push(e);
                 }
                 else if (!e.conditional && e.position === formElements.all.length) {
-                    lastQuestionIds.push(e.id);
+                    lastQuestions.push(e);
                 }
             }
             else if (e instanceof Section) {
                 let conditionalQuestions = e.questions.all.filter(q => q.conditional);
                 if (conditionalQuestions.length === 1 && conditionalQuestions[0].choices.all.filter(c => !c.next_section_id).length > 0) {
-                    lastQuestionIds.push(conditionalQuestions[0].id);
+                    lastQuestions.push(conditionalQuestions[0]);
                 }
                 else if (conditionalQuestions.length <= 0 && e.position === formElements.all.length) {
-                    lastQuestionIds.push(e.questions.all[e.questions.all.length - 1].id);
+                    lastQuestions.push(e.questions.all[e.questions.all.length - 1]);
                 }
             }
         }
-        return lastQuestionIds;
+        return lastQuestions;
     };
 
     static hasRespondedLastQuestion = async (form: Form, distribution: Distribution) : Promise<boolean> => {
@@ -44,7 +51,13 @@ export class FormElementUtils {
         let responses = new Responses();
         await formElements.sync(form.id);
         await responses.syncByDistribution(distribution.id);
-        let lastQuestionIds = FormElementUtils.getLastQuestionIds(formElements);
-        return responses.all.filter(r => lastQuestionIds.includes(r.question_id) && r.answer).length > 0;
+        let lastQuestions = FormElementUtils.getLastQuestions(formElements);
+        return responses.all.filter(r => FormElementUtils.isLastValidLastResponse(r, lastQuestions)).length > 0;
+    };
+
+    static isLastValidLastResponse = (response: Response, lastQuestions: any) : boolean => {
+        let matchingQuestions = lastQuestions.filter(q => q.id === response.question_id);
+        let question = matchingQuestions.length > 0 ? matchingQuestions[0] : null;
+        return question && (question.conditional ? !!response.answer : true);
     };
 }
