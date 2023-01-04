5c98cc8c232d56fa7d195c8bad3c5b6ad4d63d27
Quentin Périé <perieq@hotmail.fr>
2023-01-04 15:10:23 +0100

    fix(sql) : [#FOR-508] add constraints in SQL for questions and sections (#167)
    Co-authored-by: Quentin Périé <quentin.perie@cgi.com>

---
+++ formulaire/src/main/resources/sql/017-add-constraints.sql
@@ -0,0 +1,13 @@
+ALTER TABLE formulaire.question
+    ADD CONSTRAINT question_unique_form_position UNIQUE (form_id, position),
+    ADD CONSTRAINT question_unique_section_position UNIQUE (section_id, section_position),
+    ADD CONSTRAINT question_unique_matrix_position UNIQUE (matrix_id, matrix_position),
+    ADD CONSTRAINT question_check_pairing
+    CHECK (
+        (section_id IS NOT NULL AND section_position IS NOT NULL AND matrix_id IS NULL AND matrix_position IS NULL AND position IS NULL) OR
+        (matrix_id IS NOT NULL AND matrix_position IS NOT NULL AND section_id IS NULL AND section_position IS NULL AND position IS NULL) OR
+        (position IS NOT NULL AND section_id IS NULL AND section_position IS NULL AND matrix_id IS NULL AND matrix_position IS NULL)
+    );
+
+ALTER TABLE formulaire.section
+    ADD CONSTRAINT section_unique_form_position UNIQUE (form_id, position);
