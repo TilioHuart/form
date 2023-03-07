bbd9d9b7ea3908c8497196292242519339508996
Carole Payen <39626293+c4role@users.noreply.github.com>
2023-03-07 11:14:51 +0100

    fix(update-current-sharing): #FOR-561 fix duplicate issue (#210)

--- formulaire/src/main/resources/sql/020-update-current-sharing.sql
+++ formulaire/src/main/resources/sql/020-update-current-sharing.sql
@@ -1,11 +1,11 @@
 INSERT INTO formulaire.form_shares (
-    SELECT member_id, resource_id, 'fr-openent-formulaire-controllers-ResponseController|deleteByQuestionAndDistribution'
+    SELECT DISTINCT member_id, resource_id, 'fr-openent-formulaire-controllers-ResponseController|deleteByQuestionAndDistribution'
     FROM formulaire.form_shares
     WHERE action = 'fr-openent-formulaire-controllers-FormController|initResponderResourceRight'
 );
 
 INSERT INTO formulaire.form_shares (
-    SELECT member_id, resource_id, 'fr-openent-formulaire-controllers-FormController|get'
+    SELECT DISTINCT member_id, resource_id, 'fr-openent-formulaire-controllers-FormController|get'
     FROM formulaire.form_shares
     WHERE action IN ('fr-openent-formulaire-controllers-FormController|initResponderResourceRight',
         'fr-openent-formulaire-controllers-FormController|initContribResourceRight',
