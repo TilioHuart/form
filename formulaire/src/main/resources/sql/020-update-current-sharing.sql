ac13e5b1541b13af1a09d8e88d3c26f36672770a
Quentin Périé <perieq@hotmail.fr>
2023-03-09 09:29:05 +0100

    fix(sql): [#FOR-561] fix duplicate issues in updating sharing script (#212)

--- formulaire/src/main/resources/sql/020-update-current-sharing.sql
+++ formulaire/src/main/resources/sql/020-update-current-sharing.sql
@@ -1,13 +1,29 @@
+DELETE FROM formulaire.form_shares
+WHERE action = 'fr-openent-formulaire-controllers-ResponseController|deleteByQuestionAndDistribution';
+
 INSERT INTO formulaire.form_shares (
     SELECT DISTINCT member_id, resource_id, 'fr-openent-formulaire-controllers-ResponseController|deleteByQuestionAndDistribution'
     FROM formulaire.form_shares
     WHERE action = 'fr-openent-formulaire-controllers-FormController|initResponderResourceRight'
 );
 
+WITH form_shares_to_update AS (
+    SELECT DISTINCT member_id, resource_id
+    FROM formulaire.form_shares
+    WHERE action IN (
+        'fr-openent-formulaire-controllers-FormController|initResponderResourceRight',
+        'fr-openent-formulaire-controllers-FormController|initContribResourceRight',
+        'fr-openent-formulaire-controllers-FormController|initManagerResourceRight'
+    )
+),
+form_shares_already_updated AS (
+    SELECT DISTINCT member_id, resource_id
+    FROM formulaire.form_shares
+    WHERE action = 'fr-openent-formulaire-controllers-FormController|get'
+)
 INSERT INTO formulaire.form_shares (
     SELECT DISTINCT member_id, resource_id, 'fr-openent-formulaire-controllers-FormController|get'
     FROM formulaire.form_shares
-    WHERE action IN ('fr-openent-formulaire-controllers-FormController|initResponderResourceRight',
-        'fr-openent-formulaire-controllers-FormController|initContribResourceRight',
-        'fr-openent-formulaire-controllers-FormController|initManagerResourceRight')
+    WHERE (member_id, resource_id) IN (SELECT * FROM form_shares_to_update)
+    AND (member_id, resource_id) NOT IN (SELECT * FROM form_shares_already_updated)
 );
