88dc7477011c60a8434076b00e8190e7ff3ba063
Quentin Périé <perieq@hotmail.fr>
2023-06-21 11:32:16 +0200

    chore(): correct SQL privileges granted and change entcoreVersion

--- formulaire/src/main/resources/sql/025-add-column-img-question-choice.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE formulaire.question_choice
-    ADD COLUMN IF NOT EXISTS image VARCHAR;
-
-ALTER TABLE formulaire.response
-    ADD COLUMN IF NOT EXISTS image VARCHAR;
---
+++ formulaire/src/main/resources/sql/026-add-column-img-question-choice.sql
@@ -0,0 +1,5 @@
+ALTER TABLE formulaire.question_choice
+    ADD COLUMN image VARCHAR;
+
+ALTER TABLE formulaire.response
+    ADD COLUMN image VARCHAR;
